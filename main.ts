c1673e18b5915fb883751c2a717ad4dc34a96f97
Endolite <41879690+Endolite@users.noreply.github.com>
2023-05-15 22:01:47 -0400

    Lua support and OS detection
    Added Lua support and additional comments for code readability. Also added OS detection, but MacOS is still the only one that works.

--- main.ts
+++ main.ts
@@ -1,32 +1,37 @@
 import { Plugin, TAbstractFile, Editor } from "obsidian";
 import { detectNewline } from "detect-newline";
 
+
 let extDict = new Map<string, string>(); // Map of file extensions
 	extDict.set("cpp", "cpp");
 	extDict.set("java", "java");
+	extDict.set("lua", "lua");
 	extDict.set("python", "py");
 
 let cmdDict = new Map<string, string>(); // Map of terminal commands
 	cmdDict.set("cpp", "make")
 	cmdDict.set("java", "java");
+	cmdDict.set("lua", "lua");
 	cmdDict.set("python", "python3");
 
-let oneFile = new Map<string, boolean>(); // Map of whether a build file is required
+let oneFile = new Map<string, boolean>(); // Map of whether the temp file can be directly run
 	oneFile.set("cpp", false);
 	oneFile.set("java", true);
+	oneFile.set("lua", true);
 	oneFile.set("python", true);
 
 export default class Build extends Plugin {
+	os = require("os"); // Operating system
 	ext = ""; // File extension
 	cmd = ""; // Terminal command
-	oneF = false;
+	oneF = false; // Whether the temp file can be directly run
+	
   async onload() {
 		this.addCommand({
 			id: "build",
 			name: "Build",
 			editorCallback: (editor: Editor) => {
 				this.deleteFile(); // Clears the previous temp file before the new extension is identified
-
 				let sel = editor.getSelection(); // Makes a string from the user's mouse selection
 
 				if (sel.substring(0, 3) === "```" && sel.substring(sel.length - 3) === "```") { // Makes sure a valid selection is made
@@ -84,15 +89,19 @@ export default class Build extends Plugin {
 		this.addCommand({ // Opens a new terminal instance
 			id: "open-terminal",
 			name: "Open terminal",
-			callback: () =>{
+			callback: () => {
 				this.openTerminal();
 			}
 		})
   }
 
+	async onunload() {
+		this.deleteFile();
+	}
+
 	async makeFile(text: string) { // Creates the temp (with appropriate extension) file from input text
 		app.vault.create("temp." + this.ext, text);
-		console.log("File created");
+	//console.log("File created");
 	}
 
 	async deleteFile() { // Deletes the temp file(s)
@@ -100,13 +109,13 @@ export default class Build extends Plugin {
 			var file = app.vault.getAbstractFileByPath("temp");
 			if (file instanceof TAbstractFile) {
 				app.vault.delete(file);
-				console.log("Build file deleted");
+				//console.log("Build file deleted");
 			}	
 		}
 		var file = app.vault.getAbstractFileByPath("temp." + this.ext);
 		if (file instanceof TAbstractFile) {
 			app.vault.delete(file);
-			console.log("Temp file deleted");
+			//console.log("Temp file deleted");
 		}	
 		else {
 			//console.log("File does not exist");
@@ -116,25 +125,41 @@ export default class Build extends Plugin {
 	async openTerminal() { // Opens a new terminal instance and runs the temp file
 		const { exec } = require("child_process");
 		
-		let path = this.app.vault.adapter.getResourcePath("temp" + this.ext);
+		let path = this.app.vault.adapter.getResourcePath("temp" + this.ext); // Get's the path to the temp file before properly formatting it
 		//console.log("Original path: " + path);
 		let n = path.length - 1;
-		while (path.charAt(n) != '/'){
+		while (path.charAt(n) != '/') {
 			n--;
 		}
-		path = path.substring(24, n + 1) + "temp"
-		//console.log("Usable path:" + path);
-		let fullCMD = "osascript -e \'tell app \"Terminal\" to do script \"" + this.cmd + " " + path;
+		let n1 = 0;
+		let n2 = 0;
+		for (let i = 0; (i < path.length) && (n1 < 3); i++) {
+			if (path.charAt(i) == '/') {
+				n2 = i;
+				n1++;
+			}
+		}
+		path = path.substring(n2, n + 1) + "temp"
+		//console.log("Usable path: " + path);
+		// TODO make unix/powershell commands
+		//let fullCMD = "osascript -e \'tell app \"Terminal\" to do script \"" + this.cmd + " " + path;
 		//let fullCMD = "open -a Terminal && " + this.cmd + " " + path;
-		if (this.oneF) {
-			fullCMD += "." + this.ext + "\"\'";
+		let fullCMD = "";
+		if (this.os.platform() == "darwin") { // Detects if the user is on MacOS
+			fullCMD = "osascript -e \'tell app \"Terminal\" to do script \"" + this.cmd + " " + path;
+			if (this.oneF) {
+				fullCMD += "." + this.ext + "\"\'";
+			}
+			else {
+				fullCMD += " && " + path  + "\"\'";
+			}
+			// TODO bring created terminal window to the front
+			// fullCMD += "\'";
+			exec(fullCMD);
 		}
 		else {
-			fullCMD += " && " + path  + "\"\'";
+			console.log("MacOS is the only supported operating system as of now.");
 		}
-		// TODO bring created terminal window to the front
-		// fullCMD += "\'";
-		exec(fullCMD);
 	}
 
 }
